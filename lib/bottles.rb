f26ae2283cc7611ed3b07fda3d6408ed1e0366a6
Isidro López <islomar@gmail.com>
2019-01-09 23:31:21 +0100

    Use constants and change public methods order

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,23 +4,8 @@ class Bottles
     DEFAULT_MAX_NUMBER_OF_BOTTLES = 99
     DEFAULT_MIN_NUMBER_OF_BOTTLES = 0
 
-    def verse(number_of_bottles)
-        remaining_bottles = number_of_bottles - 1
-        if number_of_bottles == 0
-            return <<~VERSE
-                No more bottles of beer on the wall, no more bottles of beer.
-                Go to the store and buy some more, 99 bottles of beer on the wall.
-            VERSE
-        elsif number_of_bottles == 1
-            return <<~VERSE
-                1 bottle of beer on the wall, 1 bottle of beer.
-                Take it down and pass it around, no more bottles of beer on the wall.
-            VERSE
-        end
-        <<~VERSE
-            #{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
-            Take one down and pass it around, #{number_of_bottles - 1} bottle#{:s if remaining_bottles > 1} of beer on the wall.
-        VERSE
+    def song()
+        return verses(DEFAULT_MAX_NUMBER_OF_BOTTLES, DEFAULT_MIN_NUMBER_OF_BOTTLES)
     end
 
     def verses(max_number_of_bottles, min_number_of_bottles)
@@ -31,8 +16,23 @@ class Bottles
         return all_verses
     end
 
-    def song()
-        return verses(DEFAULT_MAX_NUMBER_OF_BOTTLES, DEFAULT_MIN_NUMBER_OF_BOTTLES)
+    def verse(number_of_bottles)
+        remaining_bottles = number_of_bottles - 1
+        if number_of_bottles == DEFAULT_MIN_NUMBER_OF_BOTTLES
+            return <<~VERSE
+                No more bottles of beer on the wall, no more bottles of beer.
+                Go to the store and buy some more, #{DEFAULT_MAX_NUMBER_OF_BOTTLES} bottles of beer on the wall.
+            VERSE
+        elsif number_of_bottles == 1
+            return <<~VERSE
+                1 bottle of beer on the wall, 1 bottle of beer.
+                Take it down and pass it around, no more bottles of beer on the wall.
+            VERSE
+        end
+        return <<~VERSE
+            #{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
+            Take one down and pass it around, #{number_of_bottles - 1} bottle#{:s if remaining_bottles > 1} of beer on the wall.
+        VERSE
     end
 
     private def verse_delimiter(current_number_of_bottles, min_number_of_bottles)
