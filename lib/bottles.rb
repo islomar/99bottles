4fb6df5339e9eec91dbc19900db3e44f7e9289c0
Isidro López <islomar@gmail.com>
2019-01-09 21:14:11 +0100

    [green] Pass verse(0)

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,12 @@
 class Bottles
     def verse(number_of_bottles)
         remaining_bottles = number_of_bottles - 1
-        if number_of_bottles == 1
+        if number_of_bottles == 0
+            return <<~HEREDOC
+                No more bottles of beer on the wall, no more bottles of beer.
+                Go to the store and buy some more, 99 bottles of beer on the wall.
+        HEREDOC
+        elsif number_of_bottles == 1
             return <<~HEREDOC
             1 bottle of beer on the wall, 1 bottle of beer.
             Take it down and pass it around, no more bottles of beer on the wall.
