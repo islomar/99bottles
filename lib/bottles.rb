d912b7d5fdafa880a398ce1e1670fcd8161b0c5d
Isidro López <islomar@gmail.com>
2019-01-09 21:02:24 +0100

    [refactor] Extract variable

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,9 @@
 class Bottles
     def verse(number_of_bottles)
+        remaining_bottles = number_of_bottles - 1
         <<~HEREDOC
             #{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
-            Take one down and pass it around, #{number_of_bottles - 1} bottle#{:s if number_of_bottles - 1 > 1} of beer on the wall.
+            Take one down and pass it around, #{number_of_bottles - 1} bottle#{:s if remaining_bottles > 1} of beer on the wall.
         HEREDOC
     end
 end
