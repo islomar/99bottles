2b4306b8515fd0d53c4d933b55f8e33dbadd0c14
Isidro López <islomar@gmail.com>
2019-01-09 21:41:12 +0100

    Rename HEREDOC to VERSE

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -2,20 +2,20 @@ class Bottles
     def verse(number_of_bottles)
         remaining_bottles = number_of_bottles - 1
         if number_of_bottles == 0
-            return <<~HEREDOC
+            return <<~VERSE
                 No more bottles of beer on the wall, no more bottles of beer.
                 Go to the store and buy some more, 99 bottles of beer on the wall.
-            HEREDOC
+            VERSE
         elsif number_of_bottles == 1
-            return <<~HEREDOC
+            return <<~VERSE
                 1 bottle of beer on the wall, 1 bottle of beer.
                 Take it down and pass it around, no more bottles of beer on the wall.
-            HEREDOC
+            VERSE
         end
-        <<~HEREDOC
+        <<~VERSE
             #{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
             Take one down and pass it around, #{number_of_bottles - 1} bottle#{:s if remaining_bottles > 1} of beer on the wall.
-        HEREDOC
+        VERSE
     end
 
     def verses(number_of_bottles1, number_of_bottles2)
