5dd7128ca161d7ad16a3b117da9b85b36d24e91c
Isidro López <islomar@gmail.com>
2019-01-09 20:43:22 +0100

    [refactor] Improve code indentation

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,8 @@
 class Bottles
     def verse(number_of_bottles)
-<<-VERSE
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
-VERSE
+        <<~HEREDOC
+            99 bottles of beer on the wall, 99 bottles of beer.
+            Take one down and pass it around, 98 bottles of beer on the wall.
+        HEREDOC
     end
 end
