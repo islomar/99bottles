1e896b64ccd80bd7ec6427d21d2d512ff1660f82
Isidro López <islomar@gmail.com>
2019-01-09 20:54:30 +0100

    [green] Pass verse(89)

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,7 @@
 class Bottles
     def verse(number_of_bottles)
         <<~HEREDOC
-            99 bottles of beer on the wall, 99 bottles of beer.
+            #{number_of_bottles} bottles of beer on the wall, 99 bottles of beer.
             Take one down and pass it around, 98 bottles of beer on the wall.
         HEREDOC
     end
