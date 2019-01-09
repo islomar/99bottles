b4621b690591f55ba30aae3eba8e33f255e1f748
Isidro López <islomar@gmail.com>
2019-01-09 21:15:50 +0100

    [refactor] Improve indentation

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,12 +5,12 @@ class Bottles
             return <<~HEREDOC
                 No more bottles of beer on the wall, no more bottles of beer.
                 Go to the store and buy some more, 99 bottles of beer on the wall.
-        HEREDOC
+            HEREDOC
         elsif number_of_bottles == 1
             return <<~HEREDOC
-            1 bottle of beer on the wall, 1 bottle of beer.
-            Take it down and pass it around, no more bottles of beer on the wall.
-        HEREDOC
+                1 bottle of beer on the wall, 1 bottle of beer.
+                Take it down and pass it around, no more bottles of beer on the wall.
+            HEREDOC
         end
         <<~HEREDOC
             #{number_of_bottles} bottles of beer on the wall, #{number_of_bottles} bottles of beer.
